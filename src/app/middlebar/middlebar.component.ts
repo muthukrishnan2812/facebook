917f5d4fbf830a241ed90f7162822fe128aef250
Muthu Krishnan <muthumk2812@gmail.com>
2024-03-19 15:14:15 +0530

    commit

--- src/app/middlebar/middlebar.component.ts
+++ src/app/middlebar/middlebar.component.ts
@@ -1,16 +1,24 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-middlebar',
   templateUrl: './middlebar.component.html',
   styleUrl: './middlebar.component.scss'
 })
-export class MiddlebarComponent {
+export class MiddlebarComponent implements OnInit{
 body:any
 todo:any
+title:string=''
+data:any[]=[]
+isVisible:any
 today:any=new Date();
 constructor(private http:HttpClient){}
+ngOnInit(): void {
+  this.getData();
+  
+}
+
 
 postData(){
   return this.http.post(`https://jsonplaceholder.typicode.com/posts/`,{body:this.body})
@@ -21,4 +29,17 @@ postData(){
     alert('data posted succesfully')
   })
  }
+ getData(){
+  return this.http.get(`https://jsonplaceholder.typicode.com/posts/`)
+  .subscribe(data=>{
+    console.log(data);
+    this.todo=data;
+  })
+ }
+ close(): void {
+  this.closeClicked.emit();
+  console.log('closebuttonclicked');
+  this.isVisible = true;
+}
+@Output() closeClicked: EventEmitter<void> = new EventEmitter<void>();
 }
